6d370de8e0dfe3ce14070e790e2ddc2865b8a74e
Guacamoleboy <twitch@galleripermoller.dk>
2025-03-02 11:52:59 +0100

    Java compiler

---
+++ java-compiler.bat
@@ -0,0 +1,24 @@
+@ECHO OFF 
+
+color e
+
+ECHO.
+ECHO Compiler By Guacamoleboy
+ECHO.
+ECHO __________________________________
+
+ECHO.
+ECHO Compiling File: %1
+javac "%~f1"
+ECHO.
+ECHO Class File Generated.
+ECHO.
+
+ECHO Running Java File: "%~n1"
+ECHO.
+ECHO __________________________________
+ECHO.
+java %~n1
+pause >nul
+
+ECHO ON
